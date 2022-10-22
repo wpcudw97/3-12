b881e764d3a7579f56f10e1156b0fe13da53e63e
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-10-22 14:22:08 +0000

    投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'books/new'
+  post 'books' => 'books#create'
   get 'books/index'
-  get 'books/show'
+  get '/show' => "homes#show"
   get 'books/edit'
-  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
