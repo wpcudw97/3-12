660542807217c2ca6841c219899b0e69920276b7
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-10-23 16:22:38 +0000

    編集機能作成index⇄show⇄edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/show' => 'homes#show'
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
