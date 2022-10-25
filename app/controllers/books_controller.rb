b5b10101469809ded86bbd22a0fc6d22e93d0e73
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-10-25 13:51:05 +0000

    index,show.html レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -31,6 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :edit
